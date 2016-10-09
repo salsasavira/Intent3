1a7478514dc911f8e778ae19fa751429658bc48a
Salsa <salsabillamegasavira@gmail.com>
2016-10-09 18:34:57 +0700

    Initial Commit Intent Telepon Intent SMS Intent Browser

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -28,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
                         composeSmsMessage("Pesan dari SMK Telkom Malang");
                     }
                 });
+
+        findViewById(R.id.imageViewBrowser)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        openWebPage("http://www.smktelkom-mlg.sch.id/");
+                    }
+                });
     }
 
     public void dialPhoneNumber(String phoneNumber) {
@@ -41,6 +49,13 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/plain");
         intent.putExtra("sms_body", message);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
+    }
+
+    public void openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
         if (intent.resolveActivity(getPackageManager()) != null)
             startActivity(intent);
     }
